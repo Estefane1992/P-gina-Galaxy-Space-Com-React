bf425a574905a1931718fc65c7d229b1e1e86d49
Estefane1992 <108195915+Estefane1992@users.noreply.github.com>
2022-12-22 15:45:46 -0300

    Add files via upload

---
+++ src/componentes/Rodape/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Rodape.module.scss";
+
+export default function Rodape() {
+  return (
+    <footer className={styles.rodape}>
+      
+      <p>Desenvolvido por Estefane</p>
+    </footer>
+  );
+}
